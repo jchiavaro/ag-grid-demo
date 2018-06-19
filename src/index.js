97d1d5d28aca892fa6473eac97fc07d51c1590a8
Jorge Chiavaro <jorge@senr.io>
2018-06-19 20:26:49 -0300

    Fix https issue with the api

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
+axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
